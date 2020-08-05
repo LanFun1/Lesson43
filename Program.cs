12dfa0e6a8506ef1a0c5f2117d56faeb88a652a9
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-08-05 17:45:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someNumber = ReadingInt();
+            Console.WriteLine("\n"+someNumber);
+        }
+
+        static string ReadingInt()
+        {
+            bool successOfExecution = false;
+            Console.WriteLine("Введите число");
+            while (successOfExecution == false)
+            {
+                string someNumber = Console.ReadLine();
+                successOfExecution = Int32.TryParse(someNumber,out int result);
+                if(successOfExecution)
+                {                   
+                    return someNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Невозможно преобразовать, введите число заново.");
+                }
+            }
+            return "";
+        }
+    }
+}
